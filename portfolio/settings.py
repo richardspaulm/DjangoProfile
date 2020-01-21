8b2390ea8aa0d67bdc0996f85f83aee09aef6748
richardspaulm <richardspaulm@gmail.com>
2020-01-21 21:39:59 +0530

    New Image

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-DEBUG=False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# DEBUG=False
 ALLOWED_HOSTS = []
 
 
